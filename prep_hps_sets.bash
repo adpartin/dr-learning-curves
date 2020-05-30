128091338f628679da6d324a43421cc6abfbfa40
Alex Partin <25892264+adpartin@users.noreply.github.com>
2020-05-30 14:10:21 -0700

    Bash script to agg keras-tuner results

---
+++ prep_hps_sets.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# The input that is assigned to "maindir" is the main output
+# dir where keras-tuner dumps the results. For example:
+# bash prep_hps_sets.bash 
+
+maindir=$1
+cd $maindir
+maindir=$(pwd)
+echo "Main dir $maindir"
+
+outdir=$maindir/lc_hp_sets
+echo "Out dir $outdir"
+mkdir -p $outdir
+
+for fname in *; do
+    if [[ "$fname" == *"tr_sz"* ]]; then
+        echo ${fname}
+        outpath=$outdir/$fname
+        mkdir -p $outpath
+        cp -r ${fname}/my_logs/* $outpath
+    fi
+done
