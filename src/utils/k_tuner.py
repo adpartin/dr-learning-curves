4dd540ca6781bf70d3074e1c4e478d60516650b7
Alex Partin <25892264+adpartin@users.noreply.github.com>
2020-06-02 12:20:03 -0700

    Add utils to handle hps params

---
+++ src/utils/k_tuner.py
@@ -0,0 +1,32 @@
+from utils.utils import verify_path
+
+def boolify(s):
+    if s=='True':
+        return True
+    if s=='False':
+        return False
+    raise ValueError("huh?")
+
+def autoconvert(s):
+    for fn in (boolify, int, float):
+        try:
+            return fn(s)
+        except ValueError:
+            pass
+    return s
+
+def read_hp_prms(hp_path):
+    dct = dict()
+    hp_path = verify_path( hp_path )
+    with open(hp_path, 'r') as file:
+        for line in file:
+            aa = line.strip().split(':')
+            # print(line)
+            # key = aa[0].strip()
+            # val = aa[1].strip()
+            # val = autoconvert( val )
+            # ml_init_kwargs[key] = val
+            dct[aa[0].strip()] = autoconvert( aa[1].strip() )
+    return dct
+
+
